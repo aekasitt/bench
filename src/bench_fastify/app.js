0539b7c757c718585e2d516e465fe99d79b5666a
Sitt Guruvanich <aekazitt@gmail.com>
2025-06-26 13:53:46 +0700

    revert: rollback to using 1 fastify worker

--- src/bench_fastify/app.js
+++ src/bench_fastify/app.js
@@ -1,7 +1,6 @@
 /* ~~/src/bench_fastify/app.js */
 
 // imports
-import cluster from 'node:cluster'
 import histogram from './metrics.js'
 import save from './devices.js'
 import { randomUUID } from 'node:crypto'
@@ -93,18 +92,8 @@ server.setNotFoundHandler((_, reply) => {
   reply.status(404).type('text/plain').send('Not Found')
 })
 
-if (cluster.isPrimary) {
-  const workers = parseInt(process.argv[2] || 4)
-  console.log(`Node is listening on http://0.0.0.0:8080 ...`)
-  console.log(`Workers=${workers}`)
-  for (let i = 0; i < workers; i++) cluster.fork()
-  cluster.on(
-    'exit',
-    (worker, code, signal) => console.log(`worker ${worker.process.pid} died`),
-  )
-} else {
-  server.listen({
-    host: '0.0.0.0',
-    port: 8080,
-  })
-}
+console.log(`Node is listening on http://0.0.0.0:8080 ...`)
+server.listen({
+  host: '0.0.0.0',
+  port: 8080,
+})
